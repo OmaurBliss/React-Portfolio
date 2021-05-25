d4d7efd4a8f4d3d8a11a0467a698be85664ce275
Omaur B <omaurbliss@gmail.com>
2021-05-25 10:09:48 -0500

    Updates

---
+++ precache-manifest.cc2d34501f79c8b649cdc184c0094b86.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5707e2caa1a12e099a4a14a634e64547",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "7117f2ba04e3915f3718",
+    "url": "/React-Portfolio/static/css/main.8fa42407.chunk.css"
+  },
+  {
+    "revision": "3b0f22b907c372bbabe5",
+    "url": "/React-Portfolio/static/js/2.3b365f3b.chunk.js"
+  },
+  {
+    "revision": "bfbf98b39da508f522b8cab91a8e0702",
+    "url": "/React-Portfolio/static/js/2.3b365f3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7117f2ba04e3915f3718",
+    "url": "/React-Portfolio/static/js/main.5baf6170.chunk.js"
+  },
+  {
+    "revision": "7c35ff808cd94c4ce20c",
+    "url": "/React-Portfolio/static/js/runtime-main.0ad08de7.js"
+  },
+  {
+    "revision": "5bfa155442e8a54cd05f3db91bd522fb",
+    "url": "/React-Portfolio/static/media/Biblioflix_team_Seinfeld.5bfa1554.png"
+  },
+  {
+    "revision": "4889be1f02dd7cef457dafcc44d5cc8a",
+    "url": "/React-Portfolio/static/media/Cup_of_Sugar_Le_Mustache.4889be1f.png"
+  },
+  {
+    "revision": "42591cc14e18929c03771cb29093d6df",
+    "url": "/React-Portfolio/static/media/Employee_Directory.42591cc1.png"
+  },
+  {
+    "revision": "5bd5ece339e3bb56613c3b0475f7d354",
+    "url": "/React-Portfolio/static/media/Note_Taker_app.5bd5ece3.png"
+  },
+  {
+    "revision": "3145ca74c20f9acdaccdf500379e94ce",
+    "url": "/React-Portfolio/static/media/OB_Web_portfolio.3145ca74.png"
+  },
+  {
+    "revision": "3cbe4afa8379398362339732acffd980",
+    "url": "/React-Portfolio/static/media/OB_portfoliothree.3cbe4afa.jpg"
+  },
+  {
+    "revision": "112c646b65382649ec6dccfa3cd0f8f4",
+    "url": "/React-Portfolio/static/media/OBavatar.112c646b.png"
+  },
+  {
+    "revision": "79294a13bfe5f3a37fc70aefedfdf006",
+    "url": "/React-Portfolio/static/media/Offline_Budget_Tracker.79294a13.png"
+  }
+]);
