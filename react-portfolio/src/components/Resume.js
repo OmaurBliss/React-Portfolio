38b8911398e1c681b37a8643146933e55849c27d
Omaur B <omaurbliss@gmail.com>
2021-05-25 08:43:30 -0500

    added resume

---
+++ react-portfolio/src/components/Resume.js
@@ -0,0 +1,201 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
+
+const useStyles = makeStyles((theme) => ({
+  mainContainer: {
+    background: "#233",
+  },
+  timeLine: {
+    position: "relative",
+    padding: "1rem",
+    margin: "o auto",
+    "&:before": {
+      content: "''",
+      position: "absolute",
+      height: "100%",
+      border: "1px solid tan",
+      right: "40px",
+      top: 0,
+    },
+    "&:after": {
+      content: "''",
+      display: "table",
+      clear: "both",
+    },
+    [theme.breakpoints.up("md")]: {
+      padding: "2rem",
+      "&:before": {
+        left: "calc(50% - 1px)",
+        right: "auto",
+      },
+    },
+  },
+  timeLineItem: {
+    padding: "1rem",
+    borderBottom: "2px solid tan",
+    position: "relative",
+    margin: "1rem 3rem 1rem 1rem",
+    clear: "both",
+    "&:after": {
+      content: "''",
+      position: "absolute",
+    },
+    "&:before": {
+      content: "''",
+      position: "absolute",
+      right: "-0.625rem",
+      top: "calc(50% - 5px)",
+      borderStyle: "solid",
+      borderColor: "tomato tomato transparent transparent",
+      borderWidth: "0.625rem",
+      transform: "rotate(45deg)",
+    },
+    [theme.breakpoints.up("md")]: {
+      width: "44%",
+      margin: "1rem",
+      "&:nth-of-type(2n)": {
+        float: "right",
+        margin: "1rem",
+        borderColor: "tan",
+      },
+      "&:nth-of-type(2n):before": {
+        right: "auto",
+        left: "-0.625rem",
+        borderColor: "transparent transparent tomato tomato",
+      },
+    },
+  },
+  timeLineYear: {
+    textAlign: "center",
+    maxWidth: "9.375rem",
+    margin: "0 3rem 0 auto",
+    fontSize: "1.8rem",
+    color: "#fff",
+    background: "tomato",
+    lineHeight: 1,
+    padding: "0.5rem 1rem",
+    "&:before": {
+      display: "none",
+    },
+    [theme.breakpoints.up("md")]: {
+      textAlign: "center",
+      margin: "0 auto",
+      "&:nth-of-type(2n)": {
+        float: "none",
+        margin: "0 auto",
+      },
+      "&:nth-of-type(2n):before": {
+        display: "none",
+      },
+    },
+  },
+  heading: {
+    color: "tomato",
+    padding: "3rem 0",
+    textTransform: "uppercase",
+  },
+  subHeading: {
+    color: "#fff",
+    padding: 0,
+    textTransform: "uppercase",
+  },
+  body1: {
+    color: "tomato",
+  },
+  subtitle1: {
+    color: "tan",
+  },
+}));
+
+const Resume = () => {
+  const classes = useStyles();
+  return (
+    <Box component="header" className={classes.mainContainer}>
+      <Typography variant="h4" align="center" className={classes.heading}>
+        Working Experience
+      </Typography>
+      <Box component="div" className={classes.timeLine}>
+        <Typography
+          variant="h2"
+          className={`${classes.timeLineYear} ${classes.timeLineItem}`}
+        >
+          2015-Present
+        </Typography>
+        <Box component="div" className={classes.timeLineItem}>
+          <Typography
+            variant="h5"
+            align="center"
+            className={classes.subHeading}
+          >
+            Product Owner/ Web Developer
+          </Typography>
+          <Typography variant="body1" align="center" className={classes.body1}>
+            Bliss Full Fit
+          </Typography>
+          <Typography
+            variant="subtitle1"
+            align="center"
+            className={classes.subtitle1}
+          >
+            Co owner of a health and wellness social impact company that works with our clients to create solutions that can enrich the lives of their program participants. Overseeing product development and using technology for program implimentation.
+          </Typography>
+        </Box>
+        <Typography
+          variant="h2"
+          className={`${classes.timeLineYear} ${classes.timeLineItem}`}
+        >
+          2018-Present
+        </Typography>
+        <Box component="div" className={classes.timeLineItem}>
+          <Typography
+            variant="h5"
+            align="center"
+            className={classes.subHeading}
+          >
+            Web development
+          </Typography>
+          <Typography variant="body1" align="center" className={classes.body1}>
+            Mic Drop Entertainment
+          </Typography>
+          <Typography
+            variant="subtitle1"
+            align="center"
+            className={classes.subtitle1}
+          >
+            Web development and app automation for a corporate entertainment company. Oversaw website development and automation solution for marketing and booking departments
+          </Typography>
+        </Box>
+        <Typography
+          variant="h2"
+          className={`${classes.timeLineYear} ${classes.timeLineItem}`}
+        >
+          2021 -
+        </Typography>
+        <Box component="div" className={classes.timeLineItem}>
+          <Typography
+            variant="h5"
+            align="center"
+            className={classes.subHeading}
+          >
+            Fullstack Javascript Developer
+          </Typography>
+          <Typography variant="body1" align="center" className={classes.body1}>
+            Mylestone LLC
+          </Typography>
+          <Typography
+            variant="subtitle1"
+            align="center"
+            className={classes.subtitle1}
+          >
+            Fullstack web developer for consulting firm that specializes in working with military contractors and health care companies.
+          </Typography>
+        </Box>
+        
+      </Box>
+    </Box>
+  );
+};
+
+export default Resume;
